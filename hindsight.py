eb8ee7c0bb51ae52f06817539822091103473b0d
John Ash <speakerjohnash@gmail.com>
2023-03-29 13:42:23 -0700

    updates

--- hindsight.py
+++ hindsight.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pandas as pd
 from datetime import datetime
 from itertools import islice
@@ -87,52 +88,79 @@ def create_daily_summaries(df):
     # This function will create summaries of thoughts, reflections, questions, and predictions
     # for each day, providing a brief overview of the main topics discussed on that day.
 
+    def create_summary(conversation):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation
+        )
+        response = response.choices[0].message.content.strip()
+        return response
+
     # Group the data by date (daily) and include all columns for each group
     df.set_index('Post date', inplace=True)
     daily_groups = df.groupby(df.index.date)
 
     summaries = []
 
-    for day_date, day in islice(daily_groups, 5):
+    for day_date, day in islice(daily_groups, 20):
         # Extract the date from the index of the DataFrame daily_groups
 
         conversation = [
-            {"role": "system", "content": "You read in a sequence of thoughts from a day summarize what that speaker was thinking about that day"},
+            {"role": "system", "content": "You read in a sequence of John Ash's thoughts from a day and summarize what he thinking about that day"},
             {"role": "system", "content": "These thoughts have metadata from a dialectic called fourthought to help contextualize the flow of cognition"},
             {"role": "system", "content": "Each thought is tagged with a thought type: prediction, statements, reflection, or question"},
             {"role": "system", "content": "Predictions, reflections and statements are about the future, past and present respectively. Each has two voting systems: truth and good"},
-            {"role": "system", "content": "Truth is a scale of 0 to 100. 50 means uncertain. If there is no vote, no one provided any certainty vote"},
+            {"role": "system", "content": "Truth is a scale of 0 to 100. 50 means uncertain. Anything below 50 means the thought is leaning false, anything over means leaning true. 75 is a medium level of certainty of truth. 25 is a medium level of certainty of falsity. If there is no vote, no one provided any certainty vote"},
             {"role": "system", "content": "You are receiving timestamps and can make inferences about the length of time between thoughts as to whether they're connected"}
         ]
-        
+
         text = f"Date: {day_date.strftime('%Y-%m-%d')}\n"
 
         day = day.sort_index()
 
         for index, row in day.iterrows():
+
+            sentiment_votes = row['Positive'] + row['Negative']
+            positivity = " " if sentiment_votes == 0 else row['Positive'] / sentiment_votes
+
+            thought_text = row['Thought']
+
             text += f"Timestamp: {index.strftime('%m/%d/%y %I:%M %p')}\n"
-            text += f"Thought: {row['Thought']}\n"
-            text += f"Sentiment: {row['Positive']} / {row['Negative']}\n"
+            text += f"Thought: {thought_text}\n"
+            text += f"Sentiment: " + str(positivity) + "\n"
             text += f"Good Votes: {row['Positive']}\n"
             text += f"Bad Votes: {row['Negative']}\n"
             text += f"Average Certainty: {row['Truth']}\n"
             text += f"Speaker: John Ash\n"
             text += f"Thought Type: {row['Type']}\n\n"
-        
+
         # Print the concatenated text for each day
-        conversation.append({"role": "user", "content": "Please summarize what this speaker was thinking about into a story in relation to their place in time. Reference anything in the dialectic helpful towards telling that story: " + text})
+        conversation.append({"role": "system", "content": "Only say what is in the text itself"})
 
-        # print(conversation)
+        # Split the text into chunks of 4000 characters or less
+        text_chunks = [text[i:i+4000] for i in range(0, len(text), 4000)]
 
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", 
-            messages=conversation
-        )
+        # Do Recursive Summarization if Too Long
+        if len(text_chunks) > 1:
 
-        response = response.choices[0].message.content.strip()
+            summaries = []
 
-        print(text + "\n")
-        print(response)
+            for chunk in text_chunks:
+                sub_conv = conversation[:]
+                sub_conv.append({"role": "user", "content": "You are summarizing part of a day this time. Please state the range of the day (TIME to TIME) that you're summarizing and then summarize what this speaker was thinking about during this section of the day (and reference the period of the day TIME to TIME) into a paragraph story in relation to their place in time. Reference anything in the dialectic helpful towards telling that story but don't make anything up: " + chunk})
+                response = create_summary(sub_conv)
+                summaries.append(response)
+
+            summary_text = '\n\n'.join([summary.strip() for summary in summaries])
+            sub_conv = [{"role": "system", "content": "You stitch summaries about parts of a day into one. You remove line breaks and make minor edits to make multiple sections into one. You mostly copy text"}]
+            sub_conv.append({"role": "user", "content": "Write this as one paragraph with no line breaks. Copy everything and miss nothing: " + summary_text})
+            response = create_summary(sub_conv)
+
+        else:
+            conversation.append({"role": "user", "content": "Please summarize what this speaker was thinking about into a story in relation to their place in time. Reference anything in the dialectic helpful towards telling that story but don't make anything up: " + text})
+            response = create_summary(conversation)
+
+        print(response + "\n")
 
 # Load and preprocess the CSV into a DataFrame
 df = load_and_preprocess_csv('prophet_thought_dump_ALL_THOUGHTS_2023.csv')
