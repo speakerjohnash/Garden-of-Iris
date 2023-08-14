52feebdc18abe182af94f934ac1c25401e4741d0
John Ash <speakerjohnash@gmail.com>
2023-08-14 14:09:48 -0700

    finally plots all day week and monthly avaialable instead of just trying to get 1 point per day or 7 points per week

--- time/spline.py
+++ time/spline.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import timedelta
 from scipy.interpolate import CubicSpline
 
 class SyntheticTimeSeriesGenerator:
@@ -30,6 +31,12 @@ class SyntheticTimeSeriesGenerator:
 
 		return smooth_base_pattern
 
+	def add_weekly_trends(self, prices):
+		days_in_week = 7	# days in a week
+		# Using a sinusoidal function to create a smooth weekly pattern
+		weekly_trend = 1 + 0.02 * np.sin(2 * np.pi * np.arange(len(prices)) / days_in_week)
+		return prices * weekly_trend
+
 	def add_seasonal_variation(self, prices):
 		days_in_year = 365.25
 		seasonal_pattern = 1 + 0.05 * np.sin(2 * np.pi * np.arange(len(prices)) / days_in_year)
@@ -37,10 +44,10 @@ class SyntheticTimeSeriesGenerator:
 
 	def add_significant_annual_events(self, prices):
 		years = np.linspace(0, len(prices), num=int(len(prices) // 365.25))
-		event_years = np.random.choice(years, size=5, replace=False)  # choosing 5 years randomly for significant events
+		event_years = np.random.choice(years, size=5, replace=False)	# choosing 5 years randomly for significant events
 		for year in event_years:
 			start, end = int(year), int(year + 365.25)
-			event_factor = np.random.uniform(0.9, 1.1)  # deviating up to 10% from base pattern
+			event_factor = np.random.uniform(0.9, 1.1)	# deviating up to 10% from base pattern
 			prices[start:end] *= event_factor
 		return prices
 
@@ -49,13 +56,13 @@ class SyntheticTimeSeriesGenerator:
 		return prices + oscillation
 
 	def add_monthly_trends(self, prices):
-		days_in_month = 30.44  # average days in a month
+		days_in_month = 30.44	# average days in a month
 		monthly_trend = 1 + 0.03 * np.sin(2 * np.pi * np.arange(len(prices)) / days_in_month)
 		return prices * monthly_trend
 
 	def add_significant_monthly_events(self, prices):
 		months = np.linspace(0, len(prices), num=int(len(prices) // (365.25/12)))
-		event_months = np.random.choice(months, size=24, replace=False)  # choosing 2 months per year for significant events
+		event_months = np.random.choice(months, size=24, replace=False)	# choosing 2 months per year for significant events
 		for month in event_months:
 			start, end = int(month), int(month + (365.25/12))
 			event_factor = np.random.uniform(0.9, 1.1)
@@ -63,7 +70,7 @@ class SyntheticTimeSeriesGenerator:
 		return prices
 
 	def add_daily_variation_within_month(self, prices):
-		day_pattern = np.random.choice([0.98, 1.02], size=len(prices))  # randomly assign a surge or drop for each day
+		day_pattern = np.random.choice([0.98, 1.02], size=len(prices))	# randomly assign a surge or drop for each day
 		return prices * day_pattern
 
 	def generate(self):
@@ -80,54 +87,70 @@ class SyntheticTimeSeriesGenerator:
 		prices = self.add_significant_monthly_events(prices)
 		prices = self.add_daily_variation_within_month(prices)
 		
+		# Adding weekly perturbations
+		prices = self.add_weekly_trends(prices)
+
 		self.data = pd.DataFrame({
 			'Date': date_range,
 			'Close': prices
 		})
 		return self.data
 
+	def save_to_csv(self):
+		self.data.to_csv('generated_data.csv', index=False)
+
 	def plot(self):
-		fig, axes = plt.subplots(3, 2, figsize=(18, 12))
-		
-		# Plot full range
-		axes[0, 0].plot(self.data['Date'], self.data['Close'])
-		axes[0, 0].set_title('Full Range')
-		axes[0, 0].grid(True)
+			fig, axes = plt.subplots(3, 2, figsize=(18, 12))
 
-		# Plot random decade (10 years)
-		start_decade = np.random.randint(0, len(self.data) - 365 * 10)
-		axes[0, 1].plot(self.data['Date'].iloc[start_decade:start_decade + 365 * 10], self.data['Close'].iloc[start_decade:start_decade + 365 * 10])
-		axes[0, 1].set_title('Random Decade')
-		axes[0, 1].grid(True)
+			# Plot full range
+			axes[0, 0].plot(self.data['Date'], self.data['Close'])
+			axes[0, 0].set_title('Full Range')
+			axes[0, 0].grid(True)
 
-		# Plot random year
-		start_year = np.random.randint(0, len(self.data) - 365)
-		axes[1, 0].plot(self.data['Date'].iloc[start_year:start_year + 365], self.data['Close'].iloc[start_year:start_year + 365])
-		axes[1, 0].set_title('Random Year')
-		axes[1, 0].grid(True)
+			# Plot random decade (10 years)
+			start_decade_idx = np.random.randint(0, len(self.data) - 365 * 10)
+			axes[0, 1].plot(self.data['Date'].iloc[start_decade_idx:start_decade_idx + 365 * 10], self.data['Close'].iloc[start_decade_idx:start_decade_idx + 365 * 10])
+			axes[0, 1].set_title('Random Decade')
+			axes[0, 1].grid(True)
 
-		# Plot random month
-		start_month = np.random.randint(0, len(self.data) - 30)
-		axes[1, 1].plot(self.data['Date'].iloc[start_month:start_month + 30], self.data['Close'].iloc[start_month:start_month + 30])
-		axes[1, 1].set_title('Random Month')
-		axes[1, 1].grid(True)
+			# Plot random year
+			start_year_idx = np.random.randint(0, len(self.data) - 365)
+			axes[1, 0].plot(self.data['Date'].iloc[start_year_idx:start_year_idx + 365], self.data['Close'].iloc[start_year_idx:start_year_idx + 365])
+			axes[1, 0].set_title('Random Year')
+			axes[1, 0].grid(True)
 
-		# Plot random week
-		start_week = np.random.randint(0, len(self.data) - 7)
-		axes[2, 0].plot(self.data['Date'].iloc[start_week:start_week + 7], self.data['Close'].iloc[start_week:start_week + 7])
-		axes[2, 0].set_title('Random Week')
-		axes[2, 0].grid(True)
+			# Plot random month
+			start_month_idx = np.random.randint(0, len(self.data) - 30)
+			start_month_date = self.data['Date'].iloc[start_month_idx]
+			end_month_date = start_month_date + timedelta(weeks=4) # Approximate a month as 4 weeks
+			month_data = self.data[(self.data['Date'] >= start_month_date) & (self.data['Date'] < end_month_date)]
+			axes[1, 1].plot(month_data['Date'], month_data['Close'])
+			axes[1, 1].set_title('Random Month')
+			axes[1, 1].grid(True)
 
-		# Plot random day
-		start_day = np.random.randint(0, len(self.data) - 1)
-		axes[2, 1].plot(self.data['Date'].iloc[start_day:start_day + 1], self.data['Close'].iloc[start_day:start_day + 1])
-		axes[2, 1].set_title('Random Day')
-		axes[2, 1].grid(True)
+			# Plot random week
+			start_week_idx = np.random.randint(0, len(self.data) - 7)
+			start_week_date = self.data['Date'].iloc[start_week_idx]
+			end_week_date = start_week_date + timedelta(weeks=1)
+			week_data = self.data[(self.data['Date'] >= start_week_date) & (self.data['Date'] < end_week_date)]
+			axes[2, 0].plot(week_data['Date'], week_data['Close'])
+			axes[2, 0].set_title('Random Week')
+			axes[2, 0].grid(True)
 
-		plt.tight_layout()
-		plt.show()
+			# Plot random day
+			start_day_idx = np.random.randint(0, len(self.data) - 1)
+			start_day_date = self.data['Date'].iloc[start_day_idx]
+			end_day_date = start_day_date + timedelta(days=1)
+			day_data = self.data[(self.data['Date'] >= start_day_date) & (self.data['Date'] < end_day_date)]
+			axes[2, 1].plot(day_data['Date'], day_data['Close'])
+			axes[2, 1].set_title('Random Day')
+			axes[2, 1].grid(True)
+
+			plt.tight_layout()
+			plt.show()
 
 # Usage
 generator = SyntheticTimeSeriesGenerator(start_date='1920-01-01', end_date='2020-01-01', num_points=500000)
 generator.generate()
 generator.plot()
+generator.save_to_csv()
