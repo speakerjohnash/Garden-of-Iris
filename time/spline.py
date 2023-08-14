91524576a74f82a85c2b6fb041005f3fc8951aaf
John Ash <speakerjohnash@gmail.com>
2023-08-14 11:48:38 -0700

    yearly perturbations

--- time/spline.py
+++ time/spline.py
@@ -5,49 +5,73 @@ from scipy.interpolate import CubicSpline
 
 class SyntheticTimeSeriesGenerator:
 
-    def __init__(self, start_date, end_date, num_points):
-        self.start_date = start_date
-        self.end_date = end_date
-        self.num_points = num_points
-        self.data = pd.DataFrame()
+	def __init__(self, start_date, end_date, num_points):
+		self.start_date = start_date
+		self.end_date = end_date
+		self.num_points = num_points
+		self.data = pd.DataFrame()
 
-    def create_base_pattern(self):
-        # Generate key time points spanning 0 to 1
-        key_time_points = np.linspace(0, 1, 20)
+	def create_base_pattern(self):
+		# Generate key time points spanning 0 to 1
+		key_time_points = np.linspace(0, 1, 20)
 
-        # Generate random key values with more locally constrained transitions
-        key_values = np.cumsum(np.random.uniform(-0.1, 0.1, 20))
-        
-        # Normalize key values to fit within [0, 1]
-        key_values = (key_values - min(key_values)) / (max(key_values) - min(key_values))
-        
-        # Adjust the range to fit within [0, 1000]
-        key_values *= 1000
+		# Generate random key values with more locally constrained transitions
+		key_values = np.cumsum(np.random.uniform(-0.1, 0.1, 20))
+		
+		# Normalize key values to fit within [0, 1]
+		key_values = (key_values - min(key_values)) / (max(key_values) - min(key_values))
+		
+		# Adjust the range to fit within [0, 1000]
+		key_values *= 1000
 
-        # Create a cubic spline interpolation for the key points
-        cubic_spline = CubicSpline(key_time_points, key_values)
-        smooth_base_pattern = cubic_spline(np.linspace(0, 1, self.num_points))
+		# Create a cubic spline interpolation for the key points
+		cubic_spline = CubicSpline(key_time_points, key_values)
+		smooth_base_pattern = cubic_spline(np.linspace(0, 1, self.num_points))
 
-        return smooth_base_pattern
+		return smooth_base_pattern
 
-    def generate(self):
-        date_range = pd.date_range(self.start_date, self.end_date, periods=self.num_points)
-        prices = self.create_base_pattern()
-        self.data = pd.DataFrame({
-            'Date': date_range,
-            'Close': prices
-        })
-        return self.data
+	def add_seasonal_variation(self, prices):
+		days_in_year = 365.25
+		seasonal_pattern = 1 + 0.05 * np.sin(2 * np.pi * np.arange(len(prices)) / days_in_year)
+		return prices * seasonal_pattern
 
-    def plot(self):
-        plt.figure(figsize=(12, 6))
-        plt.plot(self.data['Date'], self.data['Close'])
-        plt.title('Synthetic Time Series')
-        plt.xlabel('Date')
-        plt.ylabel('Close Price')
-        plt.grid(True)
-        plt.tight_layout()
-        plt.show()
+	def add_significant_annual_events(self, prices):
+		years = np.linspace(0, len(prices), num=int(len(prices) // 365.25))
+		event_years = np.random.choice(years, size=5, replace=False)  # choosing 5 years randomly for significant events
+		for year in event_years:
+			start, end = int(year), int(year + 365.25)
+			event_factor = np.random.uniform(0.9, 1.1)  # deviating up to 10% from base pattern
+			prices[start:end] *= event_factor
+		return prices
+
+	def add_yearly_oscillation(self, prices):
+		oscillation = 0.02 * np.sin(10 * 2 * np.pi * np.arange(len(prices)) / 365.25)
+		return prices + oscillation
+
+	def generate(self):
+		date_range = pd.date_range(self.start_date, self.end_date, periods=self.num_points)
+		prices = self.create_base_pattern()
+		
+		# Adding yearly perturbations
+		prices = self.add_seasonal_variation(prices)
+		prices = self.add_significant_annual_events(prices)
+		prices = self.add_yearly_oscillation(prices)
+		
+		self.data = pd.DataFrame({
+			'Date': date_range,
+			'Close': prices
+		})
+		return self.data
+
+	def plot(self):
+		plt.figure(figsize=(12, 6))
+		plt.plot(self.data['Date'], self.data['Close'])
+		plt.title('Synthetic Time Series with Yearly Perturbations')
+		plt.xlabel('Date')
+		plt.ylabel('Close Price')
+		plt.grid(True)
+		plt.tight_layout()
+		plt.show()
 
 # Usage
 generator = SyntheticTimeSeriesGenerator(start_date='1920-01-01', end_date='2020-01-01', num_points=365*100)
