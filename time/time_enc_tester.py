280924f70fda9bdfc4eb4c765262c147d20f0aea
John Ash <speakerjohnash@gmail.com>
2023-08-19 15:01:27 -0700

    getting the context window function according to expectations

--- time/time_enc_tester.py
+++ time/time_enc_tester.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -16,8 +18,8 @@ import mixed_time_enc as iris
 
 # Null Encoder
 class NullEncoder:
-    def encode(self, start_date, date):
-        return torch.tensor([])
+	def encode(self, start_date, date):
+		return torch.tensor([])
 
 # Initialize encoders with appropriate parameters
 t2v_encoder = Time2VecEncoder(in_features=1, out_features=6)
@@ -35,33 +37,48 @@ df['Date'] = pd.to_datetime(df['Date'])
 df = df[['Date', 'Close']]
 
 # Option to use ordered data (True) or random selection (False)
-use_ordered_data = True
+use_ordered_data = False
 ensure_preceding_day = True
 
-# Create sliding windows with a 30-day future target
-window_size = 50
+# Create sliding windows with a future target or randomly select from all data
+window_size = 30
 gap = 1  # The gap between the context window and the target
 X, y = [], []
+i = 0
 
 for target_idx in range(window_size, len(df) - gap):
 
-    # Create a sliding window of the 50 days preceding the target date
-    if use_ordered_data:
-        preceding_idxs = range(target_idx - window_size, target_idx)
-    else:
-        # Randomly sample 49 dates from all available preceding dates (excluding the day right before the target)
-        preceding_idxs = np.random.choice(range(target_idx - 1), window_size - 1, replace=False)
+	# Create a sliding window of the window_size days preceding the target date
+	if use_ordered_data:
+		preceding_idxs = range(target_idx - window_size, target_idx)
+	else:
+		all_preceding_data = range(target_idx) # All data before the target date
+		if ensure_preceding_day:
+			random_sample_size = window_size - 1
+			preceding_idxs = np.random.choice(all_preceding_data[:-1], random_sample_size, replace=False)
+			preceding_idxs = np.append(preceding_idxs, target_idx - 1)
+		else:
+			preceding_idxs = np.random.choice(all_preceding_data, window_size, replace=False)
+		np.random.shuffle(preceding_idxs)  # Shuffle to randomize the position of the preceding day
 
-        # If ensure_preceding_day flag is set, include the day right before the target date
-        if ensure_preceding_day:
-            preceding_idxs = np.append(preceding_idxs, target_idx - 1)
-            np.random.shuffle(preceding_idxs)  # Shuffle to randomize the position of the preceding day
-    
-    preceding_closes = df.loc[preceding_idxs, 'Close'].values
-    target_close = df.loc[target_idx + gap, 'Close']
+	# Print the preceding dates
+	preceding_dates = df.loc[preceding_idxs, 'Date'].values
+	target_date = df.loc[target_idx + gap, 'Date']
 
-    X.append(preceding_closes)
-    y.append(target_close)
+	if i == 1 or i == 9000:
+
+		print("Context window dates:")
+		print(preceding_dates)
+		print("Target date:")
+		print(target_date)
+		
+	preceding_closes = df.loc[preceding_idxs, 'Close'].values
+	target_close = df.loc[target_idx + gap, 'Close']
+
+	X.append(preceding_closes)
+	y.append(target_close)
+
+	i = i + 1
 
 X, y = np.array(X), np.array(y)
 
@@ -90,16 +107,16 @@ X_test_null = torch.stack([null_encoder.encode(start_date, date) for date in X_t
 
 # Dictionary mapping encoders to their corresponding datasets
 encoder_data = {
-    'Time2Vec': (X_train_t2v, X_test_t2v),
-    'SinusoidalBasic': (X_train_basic, X_test_basic),
-    'SinusoidalFrequencies': (X_train_freq, X_test_freq),
-    'MixedTime': (X_train_iris, X_test_iris),
-    'Null': (X_train_null, X_test_null)
+	'Time2Vec': (X_train_t2v, X_test_t2v),
+	'SinusoidalBasic': (X_train_basic, X_test_basic),
+	'SinusoidalFrequencies': (X_train_freq, X_test_freq),
+	'MixedTime': (X_train_iris, X_test_iris),
+	'Null': (X_train_null, X_test_null)
 }
 
 # List of encoders you want to include
 encoders = ['Time2Vec', 'SinusoidalBasic', 'SinusoidalFrequencies', 'MixedTime', 'Null']
-encoders = ['Time2Vec', 'SinusoidalBasic', 'Null']
+encoders = ['SinusoidalBasic', 'Null']
 
 # Initialize empty lists for training and testing datasets
 X_trains = []
@@ -107,87 +124,87 @@ X_tests = []
 
 # Append corresponding datasets for selected encoders
 for encoder in encoders:
-    train, test = encoder_data[encoder]
-    X_trains.append(train)
-    X_tests.append(test)
+	train, test = encoder_data[encoder]
+	X_trains.append(train)
+	X_tests.append(test)
 
 for i, (X_train_encoded, X_test_encoded) in enumerate(zip(X_trains, X_tests)):
   
-    print()
-    print(f"Results for {encoders[i]}:")
+	print()
+	print(f"Results for {encoders[i]}:")
 
-    size = X_train_encoded.shape[-1]
+	size = X_train_encoded.shape[-1]
 
-    print(f"Encoding shape: {X_train_encoded.shape}")
-    print(f"Encoding size: {size}")
-    print(f"X_train shape: {X_train.shape}")
+	print(f"Encoding shape: {X_train_encoded.shape}")
+	print(f"Encoding size: {size}")
+	print(f"X_train shape: {X_train.shape}")
 
-    # Flatten the encoded data along all dimensions except the first (batch)
-    X_train_flat = X_train_encoded.flatten(start_dim=1)
-    X_test_flat = X_test_encoded.flatten(start_dim=1)
+	# Flatten the encoded data along all dimensions except the first (batch)
+	X_train_flat = X_train_encoded.flatten(start_dim=1)
+	X_test_flat = X_test_encoded.flatten(start_dim=1)
 
-    print(f"X_train_flat shape: {X_train_flat.shape}")
-    print(f"X_test_flat shape: {X_test_flat.shape}")
+	print(f"X_train_flat shape: {X_train_flat.shape}")
+	print(f"X_test_flat shape: {X_test_flat.shape}")
 
-    # Convert to numpy arrays
-    X_train_flat = X_train_flat.detach().numpy()
-    X_test_flat = X_test_flat.detach().numpy()
+	# Convert to numpy arrays
+	X_train_flat = X_train_flat.detach().numpy()
+	X_test_flat = X_test_flat.detach().numpy()
 
-    print(f"X_train_flat dtype: {X_train_flat.dtype}")
-    print(f"X_test_flat dtype: {X_test_flat.dtype}")
+	print(f"X_train_flat dtype: {X_train_flat.dtype}")
+	print(f"X_test_flat dtype: {X_test_flat.dtype}")
 
-    # Concatenate encoded data with original data
-    X_train_combined = np.hstack([X_train, X_train_flat])
-    X_test_combined = np.hstack([X_test, X_test_flat])
+	# Concatenate encoded data with original data
+	X_train_combined = np.hstack([X_train, X_train_flat])
+	X_test_combined = np.hstack([X_test, X_test_flat])
 
-    print(f"X_train_combined shape: {X_train_combined.shape}")
-    print(f"X_test_combined shape: {X_test_combined.shape}")
+	print(f"X_train_combined shape: {X_train_combined.shape}")
+	print(f"X_test_combined shape: {X_test_combined.shape}")
 
-    # Convert to tensors
-    X_train_tensor = torch.tensor(X_train_combined, dtype=torch.float32)
-    X_test_tensor = torch.tensor(X_test_combined, dtype=torch.float32)
-    y_train_tensor = torch.tensor(y_train, dtype=torch.float32).view(-1, 1)
-    y_test_tensor = torch.tensor(y_test, dtype=torch.float32).view(-1, 1)
+	# Convert to tensors
+	X_train_tensor = torch.tensor(X_train_combined, dtype=torch.float32)
+	X_test_tensor = torch.tensor(X_test_combined, dtype=torch.float32)
+	y_train_tensor = torch.tensor(y_train, dtype=torch.float32).view(-1, 1)
+	y_test_tensor = torch.tensor(y_test, dtype=torch.float32).view(-1, 1)
 
-    print(f"X_train_tensor shape: {X_train_tensor.shape}")
-    print(f"X_test_tensor shape: {X_test_tensor.shape}")
-    print(f"y_train_tensor shape: {y_train_tensor.shape}")
-    print(f"y_test_tensor shape: {y_test_tensor.shape}")
+	print(f"X_train_tensor shape: {X_train_tensor.shape}")
+	print(f"X_test_tensor shape: {X_test_tensor.shape}")
+	print(f"y_train_tensor shape: {y_train_tensor.shape}")
+	print(f"y_test_tensor shape: {y_test_tensor.shape}")
 
-    model = nn.Sequential(
-        nn.Linear(X_train_combined.shape[1], 128),
-        nn.ReLU(),
-        nn.Linear(128, 64),
-        nn.ReLU(),
-        nn.Linear(64, 1)
-    )
+	model = nn.Sequential(
+		nn.Linear(X_train_combined.shape[1], 128),
+		nn.ReLU(),
+		nn.Linear(128, 64),
+		nn.ReLU(),
+		nn.Linear(64, 1)
+	)
 
-    # model = TimeEncodingTransformer(input_dim=X_train_combined.shape[1])
+	# model = TimeEncodingTransformer(input_dim=X_train_combined.shape[1])
 
-    optimizer = Adam(model.parameters(), lr=0.001)
-    loss_fn = nn.MSELoss()
+	optimizer = Adam(model.parameters(), lr=0.001)
+	loss_fn = nn.MSELoss()
 
-    # Train
-    model.train()
-    epochs = 1000
+	# Train
+	model.train()
+	epochs = 100
 
-    for epoch in range(epochs):
-        optimizer.zero_grad()
-        predictions = model(X_train_tensor)
-        loss = loss_fn(predictions, y_train_tensor)
-        loss.backward()
-        optimizer.step()
+	for epoch in range(epochs):
+		optimizer.zero_grad()
+		predictions = model(X_train_tensor)
+		loss = loss_fn(predictions, y_train_tensor)
+		loss.backward()
+		optimizer.step()
 
-        # Print the loss every 10 epochs
-        if epoch % 10 == 0:
-            print()
-            print(f"Epoch {epoch}/{epochs}, Loss: {loss.item()}")
+		# Print the loss every 10 epochs
+		if epoch % 10 == 0:
+			print()
+			print(f"Epoch {epoch}/{epochs}, Loss: {loss.item()}")
 
-    # Evaluate
-    model.eval()
-    preds = model(X_test_tensor).detach().numpy()
-    mse = mean_squared_error(y_test, preds)
-    r2 = r2_score(y_test, preds)
+	# Evaluate
+	model.eval()
+	preds = model(X_test_tensor).detach().numpy()
+	mse = mean_squared_error(y_test, preds)
+	r2 = r2_score(y_test, preds)
 
-    print('Test MSE:', mse)
-    print('Test R-squared:', r2)
+	print('Test MSE:', mse)
+	print('Test R-squared:', r2)
