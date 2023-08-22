2895f0febc7eb066c249593ca65f0bac92f4d2ba
John Ash <speakerjohnash@gmail.com>
2023-08-22 15:53:01 -0700

    fixing variable names to reflect what it's doing

--- time/pos_unix_enc.py
+++ time/pos_unix_enc.py
@@ -3,9 +3,11 @@ from datetime import datetime
 from math import sin, cos, pi
 import pandas as pd
 
+from calendar import monthrange
+
 class UnixTimePositionalEncoder:
 
-    def __init__(self, d_model=8):
+    def __init__(self, d_model=1):
         self.d_model = d_model
 
     def encode(self, start_date, claim_date):
@@ -13,24 +15,27 @@ class UnixTimePositionalEncoder:
         # No need to convert strings to datetimes
 
         time_diff = (claim_date - start_date).total_seconds()
+
+        # Get the number of days in the claim date's month
+        days_in_month = monthrange(claim_date.year, claim_date.month)[1]
         
         # Nested cyclic encoding 
-        minute_of_hour = [sin(time_diff * 2*pi / 60)] * self.d_model + [cos(time_diff * 2*pi / 60)] * self.d_model
-        hour_of_day = [sin(time_diff * 2*pi / 3600)] * self.d_model + [cos(time_diff * 2*pi / 3600)] * self.d_model   
-        day_of_month = [sin(time_diff * 2*pi / 86400)] * self.d_model + [cos(time_diff * 2*pi / 86400)] * self.d_model
-        month_of_year = [sin(time_diff * 2*pi / 2592000)] * self.d_model + [cos(time_diff * 2*pi / 2592000)] * self.d_model
-        year_of_1000 = [sin(time_diff * 2*pi / 31536000)] * self.d_model + [cos(time_diff * 2*pi / 31536000)] * self.d_model
+        cycle_of_minute = [sin(time_diff * 2*pi / 60)] * self.d_model + [cos(time_diff * 2*pi / 60)] * self.d_model
+        cycle_of_hour = [sin(time_diff * 2*pi / 3600)] * self.d_model + [cos(time_diff * 2*pi / 3600)] * self.d_model   
+        cycle_of_day = [sin(time_diff * 2*pi / 86400)] * self.d_model + [cos(time_diff * 2*pi / 86400)] * self.d_model
+        cycle_of_month = [sin(time_diff * 2*pi / 2592000)] * self.d_model + [cos(time_diff * 2*pi / 2592000)] * self.d_model
+        cycle_of_year = [sin(time_diff * 2*pi / 31536000)] * self.d_model + [cos(time_diff * 2*pi / 31536000)] * self.d_model
 
-        stacked_enc = minute_of_hour + hour_of_day + day_of_month + month_of_year + year_of_1000
+        stacked_enc = cycle_of_minute + cycle_of_hour + cycle_of_day + cycle_of_month + cycle_of_year
 
         return torch.tensor(stacked_enc)
 
 if __name__ == '__main__':
 
-    encoder = UnixTimePositionalEncoder(d_model=32)
+    encoder = UnixTimePositionalEncoder(d_model=1)
 
-    start_date = datetime(2023, 1, 1)
-    claim_date = datetime(2023, 2, 1)
+    start_date = datetime(1776, 7, 4)
+    claim_date = datetime(1863, 8, 19, 11, 2)
 
     encoded = encoder.encode(start_date, claim_date)
 
